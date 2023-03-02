6cc8ef6813c94a7c2f40b37511546c3d95e1f06d
shantdashjian <shantgdashjian@gmail.com>
2023-03-02 20:21:49 +0400

    Add functionality in JavaScript to generate 2 passwords, each 15 character long

---
+++ index.js
@@ -0,0 +1,24 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+    "/"];
+
+const generatePasswordsBtn = document.getElementById("generate-passwords-btn")
+const firstPasswordEl = document.getElementById("first-password-el")
+const secondPasswordEl = document.getElementById("second-password-el")
+
+generatePasswordsBtn.addEventListener('click', (event) => {
+    firstPasswordEl.textContent = generatePassword()
+    secondPasswordEl.textContent = generatePassword()
+})
+
+function generatePassword() {
+    let password = ""
+    const maxIndexInclusive = characters.length - 1;
+    for (let i = 0; i < 15; i++) {
+        password += characters[getRandomNumberBetween(0, maxIndexInclusive)]
+    }
+    return password
+}
+
+function getRandomNumberBetween(minIndexInclusive, maxIndexInclusive) {
+    return Math.floor(Math.random() * (maxIndexInclusive - minIndexInclusive + 1)) + minIndexInclusive
+}
